3179b32c6e9b2a7dcdc422473cc9d74c3745049d
Rafael <rr65887@gmail.com>
2023-09-18 22:47:17 -0300

    enviando função do gato frances

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,7 +6,15 @@ import cat3 from "../assets/cat3.png"
 import cat4 from "../assets/cat4.png"
 import cat5 from "../assets/cat5.png"
 
+import cat1g from "../assets/cat1g.jpg"
+import cat2g from "../assets/cat2g.jpg"
+import cat3g from "../assets/cat3g.jpg"
+import cat4g from "../assets/cat4g.jpg"
+import cat5g from "../assets/cat5g.jpg"
+
 const Counter = () => {
+
+    // Gato normal
 
     let gato1 = <img className="imgCat" src={cat1} alt='gato1' />
     let gato2 = <img className="imgCat" src={cat2} alt='gato2' />
@@ -14,11 +22,30 @@ const Counter = () => {
     let gato4 = <img className="imgCat" src={cat4} alt='gato4' />
     let gato5 = <img className="imgCat" src={cat5} alt='gato5' />
 
+
+    // Gato Frances
+
+    let gato1g = <img className='imgCat' src={cat1g} alt='gato1g' />
+    let gato2g = <img className='imgCat' src={cat2g} alt='gato2g' />
+    let gato3g = <img className='imgCat' src={cat3g} alt='gato3g' />
+    let gato4g = <img className='imgCat' src={cat4g} alt='gato4g' />
+    let gato5g = <img className='imgCat' src={cat5g} alt='gato5g' />
+
     let [count, useCount] = useState(0)
 
     let [jump, setjump] = useState(0)
 
     let [img, setimg] = useState(0)
+
+    let [french, setFrench] = useState(0)
+
+    function frenchOn() {
+        if (french == 0) {
+            setFrench(french + 1)
+        } else if (french == 1) {
+            setFrench(french = 0)
+        }
+    }
 
     function moreOne() {
         useCount(count + 1)
@@ -39,8 +66,27 @@ const Counter = () => {
             return <p>{gato5}</p>
         }
         else {
-                setimg(img = 0)
-                setjump(jump + 1)
+            setimg(img = 0)
+            setjump(jump + 1)
+        }
+    }
+
+    function jumpFrench() {
+        if (img === 0) {
+            return <p>{gato1g}</p>
+
+        } else if (img === 1) {
+            return <p>{gato2g}</p>
+        } else if (img === 2) {
+            return <p>{gato3g}</p>
+        } else if (img === 3) {
+            return <p>{gato4g}</p>
+        } else if (img === 4) {
+            return <p>{gato5g}</p>
+        }
+        else {
+            setimg(img = 0)
+            setjump(jump + 1)
         }
     }
 
@@ -50,8 +96,9 @@ const Counter = () => {
             <h1>{count}</h1>
             <h2>Pulos</h2>
             <h2>{jump}</h2>
-            {jumpImg()}
+            {french === 0 ? jumpImg() : jumpFrench()}
             <button className='main-button' onClick={moreOne}>PULAR</button>
+            <button className='french-button' onClick={frenchOn}>Gato Francês</button>
         </div>
     )
 }
